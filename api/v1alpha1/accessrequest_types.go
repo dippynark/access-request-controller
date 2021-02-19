abf6b3abe80ed0a899dffd2fb0a7175082b07b06
Luke Addison <lukeaddison785@gmail.com>
2021-02-19 11:28:28 +0000

    Implement AccessRequest API

--- api/v1alpha1/accessrequest_types.go
+++ api/v1alpha1/accessrequest_types.go
@@ -17,25 +17,42 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AccessRequestSpec defines the desired state of AccessRequest
 type AccessRequestSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of AccessRequest. Edit AccessRequest_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Subjects holds references to the objects the role applies to.
+	Subjects []rbacv1.Subject `json:"subjects,omitempty"`
+	// RoleRef can reference a Role in the current namespace or a ClusterRole in the global namespace.
+	RoleRef rbacv1.RoleRef `json:"roleRef"`
 }
 
 // AccessRequestStatus defines the observed state of AccessRequest
 type AccessRequestStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+
+	// Signifies who created the accessrequest
+	CreatedBy string `json:"createdBy,omitempty"`
+
+	// Signifies who approved the accessrequest
+	ApprovedBy string `json:"approvedBy,omitempty"`
+
+	// Represents time when the accessrequest was approved.
+	// +optional
+	ApprovalTime *metav1.Time `json:"approvalTime,omitempty"`
+
+	// Represents time when the accessrequest was completed. The completion time is only set when the
+	// accessrequest is rejected or is approved and the corresponding binding created.
+	// +optional
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+
+	// The latest available observations of an object's current state.
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []AccessRequestCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 }
 
 // +kubebuilder:object:root=true
@@ -56,6 +73,35 @@ type AccessRequestList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []AccessRequest `json:"items"`
+}
+
+type AccessRequestType string
+
+// These are valid conditions of an accessrequest.
+const (
+	// AccessRequestApproved means the accessrequest has been approved.
+	AccessRequestApproved AccessRequestType = "Approved"
+	// AccessRequestComplete means the accessrequest has completed its lifecycle.
+	AccessRequestComplete AccessRequestType = "Complete"
+)
+
+type AccessRequestCondition struct {
+	// Type of accessrequest condition, Approved or Complete.
+	Type AccessRequestType `json:"type"`
+	// Status of the condition, one of True, False, Unknown.
+	Status v1.ConditionStatus `json:"status"`
+	// Last time the condition was checked.
+	// +optional
+	LastProbeTime metav1.Time `json:"lastProbeTime,omitempty"`
+	// Last time the condition transit from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+	// (brief) reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+	// Human readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
 }
 
 func init() {
